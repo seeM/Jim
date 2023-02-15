df84683d028bf32e3bf5c42e28f314cdeb562f5c
seem <mwlorgat@gmail.com>
2023-02-15 14:07:05 +0200

    Initial Commit

---
+++ Jim/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Jim
+//
+//  Created by Wasim Lorgat on 2023/02/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
