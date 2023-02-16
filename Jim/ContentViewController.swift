0d6f7bc0cf92925bb818f746cf9d4250fbe5fc72
seem <mwlorgat@gmail.com>
2023-02-16 13:24:20 +0200

    clickable folders

--- Jim/ContentViewController.swift
+++ Jim/ContentViewController.swift
@@ -2,18 +2,26 @@ import Cocoa
 
 class ContentViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     @IBOutlet var tableView: NSTableView!
+    var jupyter: JupyterService!
     var content: Content?
     var contents = [Content]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let jupyter = JupyterService()
+        jupyter = JupyterService()
         Task {
             _ = await jupyter.login(baseUrl: "http://localhost:8999/", token: "testtoken123")
-            let path = ""
+            getContent()
+        }
+        tableView.action = #selector(onClick)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func getContent(_ path: String = "") {
+        Task {
             switch await jupyter.getContent(path, type: Content.self) {
             case .success(let content):
-                print("Got content")
                 self.content = content
                 self.contents = content.content?.sorted() ?? []
                 tableView.reloadData()
@@ -23,26 +31,24 @@ class ContentViewController: NSViewController, NSTableViewDelegate, NSTableViewD
         }
     }
     
+    // See: https://stackoverflow.com/questions/18560509/nstableview-detecting-a-mouse-click-together-with-the-row-and-column
+    @objc private func onClick() {
+        let item = contents[tableView.clickedRow]
+        if item.type == .directory {
+            getContent(item.path)
+        }
+    }
+    
     func numberOfRows(in tableView: NSTableView) -> Int {
         return self.contents.count
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         guard let view = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
-//        let string = NSMutableAttributedString(string: "")
-//        let imageAttachment = NSTextAttachment()
-//        imageAttachment.image = NSImage(systemSymbolName: "doc", accessibilityDescription: nil)!
-//        let imageString = NSAttributedString(attachment: imageAttachment)
-//        string.append(imageString)
-//        string.append(NSAttributedString(string: " " + contents[row].name))
-//        view.textField?.attributedStringValue = string
         let item = contents[row]
         let systemSymbolName = item.type == .directory ? "folder" : item.type == .notebook ? "text.book.closed" : "doc"
         view.imageView?.image = NSImage(systemSymbolName: systemSymbolName, accessibilityDescription: nil)!
-        view.textField?.stringValue = contents[row].name
-//        let imageView = NSImageView(image: image)
-//        view.textField?.addSubview(imageView)
-//        view.imageView?.image = NSImage(systemSymbolName: "doc", accessibilityDescription: nil)
+        view.textField?.stringValue = item.name
         return view
     }
 }
