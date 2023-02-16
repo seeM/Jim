ab1c7da64beecd65052f7cc42637422a65a6c0fa
seem <mwlorgat@gmail.com>
2023-02-16 14:48:30 +0200

    back button

--- Jim/ContentViewController.swift
+++ Jim/ContentViewController.swift
@@ -2,9 +2,20 @@ import Cocoa
 
 class ContentViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     @IBOutlet var tableView: NSTableView!
+    @IBOutlet var backButton: NSButton!
     var jupyter: JupyterService!
-    var content: Content?
+    var content: Content? {
+        didSet {
+            contents = content?.content?.sorted() ?? []
+            tableView.reloadData()
+        }
+    }
     var contents = [Content]()
+    var path = [Content]() {
+        didSet {
+            backButton.isEnabled = path.count > 0
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,9 +24,13 @@ class ContentViewController: NSViewController, NSTableViewDelegate, NSTableViewD
             _ = await jupyter.login(baseUrl: "http://localhost:8999/", token: "testtoken123")
             getContent()
         }
-        tableView.action = #selector(onClick)
+        tableView.action = #selector(onTableClick)
         tableView.delegate = self
         tableView.dataSource = self
+        backButton.target = self
+        backButton.action = #selector(onBackClick)
+        _ = backButton.leadingAnchor.constraint(equalTo: tableView.leadingAnchor)
+        backButton.isEnabled = false
     }
     
     func getContent(_ path: String = "") {
@@ -23,20 +38,26 @@ class ContentViewController: NSViewController, NSTableViewDelegate, NSTableViewD
             switch await jupyter.getContent(path, type: Content.self) {
             case .success(let content):
                 self.content = content
-                self.contents = content.content?.sorted() ?? []
-                tableView.reloadData()
             case .failure(let error):
                 print("Error getting content for path '\(path)':", error)
             }
         }
     }
     
+    @objc private func onBackClick() {
+        content = path.popLast()
+    }
+    
     // See: https://stackoverflow.com/questions/18560509/nstableview-detecting-a-mouse-click-together-with-the-row-and-column
-    @objc private func onClick() {
+    @objc private func onTableClick() {
         if tableView.clickedRow == -1 { return }
         let item = contents[tableView.clickedRow]
         if item.type == .directory {
+            let prev = content
             getContent(item.path)
+            if let prev {
+                path.append(prev)
+            }
         }
     }
     
