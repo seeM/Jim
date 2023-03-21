01675b6acbc8e79e045f6ec76364c4ef800ca1d6
seem <mwlorgat@gmail.com>
2023-03-21 09:29:47 +0200

    undo layout regression

--- Jim/Cell/OutputStackView.swift
+++ Jim/Cell/OutputStackView.swift
@@ -3,6 +3,10 @@ import Cocoa
 class OutputStackView: NSStackView {
     override func addArrangedSubview(_ view: NSView) {
         super.addArrangedSubview(view)
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalTo: widthAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+        ])
         view.setContentHuggingPriority(.required, for: .vertical)
     }
 }
