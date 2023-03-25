f4e178afa1c3b7748a317567f0db9f2356479c88
seem <mwlorgat@gmail.com>
2023-03-25 14:11:48 +0200

    subclass minimaltextview

--- Jim/Source/SourceTextView.swift
+++ Jim/Source/SourceTextView.swift
@@ -1,43 +1,27 @@
 import AppKit
 
-public class SourceTextView: NSTextView {
-    init() {
-        let textStorage = NSTextStorage()
-        let layoutManager = NSLayoutManager()
-        let textContainer = NSTextContainer()
-        textStorage.addLayoutManager(layoutManager)
-        layoutManager.addTextContainer(textContainer)
-        super.init(frame: .zero, textContainer: textContainer)
-    
+class SourceTextView: MinimalTextView {
+    override init() {
+        super.init()
         font = Theme.shared.font
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    public override var intrinsicContentSize: NSSize {
-        guard let textContainer = textContainer,
-              let layoutManager = layoutManager else {
-            return .zero
-        }
-        layoutManager.ensureLayout(for: textContainer)
-        let size = layoutManager.usedRect(for: textContainer).size
-        return .init(width: size.width + 2 * textContainerInset.width, height: size.height + 2 * textContainerInset.height)
-    }
 
-    public override func invalidateIntrinsicContentSize() {
+    override func invalidateIntrinsicContentSize() {
         // TODO: needed?
         super.invalidateIntrinsicContentSize()
         enclosingScrollView?.invalidateIntrinsicContentSize()
     }
 
-    public override func didChangeText() {
+    override func didChangeText() {
         super.didChangeText()
         invalidateIntrinsicContentSize()
     }
     
-    public override func becomeFirstResponder() -> Bool {
+    override func becomeFirstResponder() -> Bool {
         let sourceView = enclosingScrollView as! SourceView
         sourceView.delegate?.didBecomeFirstResponder(sourceView)
         return super.becomeFirstResponder()
